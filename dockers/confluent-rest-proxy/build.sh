9d2d8b12d9c6650dd9edc789142ecf96f43e49f0
Andrés Gómez <andresgomezfrr@gmail.com>
2017-06-12 17:25:50 +0200

    MVP-367 Change the own kafka-rest docker to the offical docker image
    * Remove confluent-rest docker build

--- dockers/confluent-rest-proxy/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build -t gcr.io/wizzie-registry/prozzie-rest-proxy:3.2.1-1 .
--- dockers/confluent-rest-proxy/kafka-rest-start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-envsubst < /etc/kafka-rest/kafka-rest_env.properties > /etc/kafka-rest/kafka-rest.properties
-/usr/bin/kafka-rest-start /etc/kafka-rest/kafka-rest.properties
