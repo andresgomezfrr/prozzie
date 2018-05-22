021518f3b56f7e9f53892d8976d4853c85526a1a
Eugenio Pérez <eupm90@gmail.com>
2018-05-22 11:13:02 +0200

    Add CLI kafka produce/consume test
    Youtrack related issues:
    WIZZIE-1764 Test ubuntu16.04 installation & kafka produce and consume cli
    behavior

--- tests/tests.sh
+++ tests/tests.sh
@@ -10,4 +10,35 @@ test_help () {
 	assertTrue $?
 }
 
+wait_for_kafka_consumer_ready () {
+	while ! grep \
+	  'Error while fetching metadata with correlation id.*LEADER_NOT_AVAILABLE' \
+	  "$1" >/dev/null; do
+	  	:
+	 done
+}
+
+test_internal_kafka () {
+	set -e
+	# We should be able to produce & consume from kafka
+	declare kafka_topic message COPROC COPROC_PID
+	declare -r expected_message='{"my":"message"}'
+	kafka_topic=$(mktemp ptXXXXXXXXXX)
+
+	coproc { "${PROZZIE_PREFIX}/bin/prozzie" kafka consume "${kafka_topic}"; } \
+							2>consume.stderr;
+
+	wait_for_kafka_consumer_ready consume.stderr
+
+	printf '%s\n' "$expected_message" | \
+		"${PROZZIE_PREFIX}/bin/prozzie" kafka produce "${kafka_topic}"
+
+	IFS= read -ru "${COPROC[0]}" message
+
+	assertEquals "${expected_message}" "${message}"
+
+	rkill "$COPROC_PID" >/dev/null
+	set +e
+}
+
 . /usr/bin/shunit2
